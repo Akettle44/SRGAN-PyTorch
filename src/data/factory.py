b75a70116c0c1509c7c378aa4dcda0e3ce214b6b
stevendee37 <stevenwchang01@gmail.com>
2024-11-16 19:37:16 -0800

    Implemented task factory for datasets

---
+++ src/data/factory.py
@@ -0,0 +1,12 @@
+# This file provides object instantiation for different datasets
+
+from .cifar10 import CIFAR10Dataset
+
+class TaskFactory():
+    @staticmethod
+    def createTaskDataSet(task_name, root_dir, kernel_size, sigma, size, batch_size, num_workers):
+        match task_name:
+            case "CIFAR10":
+                return CIFAR10Dataset(root_dir, kernel_size, sigma, size, batch_size, num_workers)
+            case _:
+                raise ValueError(f"Task: {task_name} is not currently supported")
---
+++ test/test_data/test_factory.py
@@ -0,0 +1,20 @@
+import pytest
+import os
+from src.data.factory import TaskFactory
+from src.data.cifar10 import CIFAR10Dataset
+
+@pytest.mark.usefixtures("setUp")
+class TestTaskFactory():
+    def testCIFAR10(self, setUp):
+        """ Test that CIFAR10 object is instantiated correctly
+        """
+        task_name = "CIFAR10"
+        root_dir = os.getcwd()
+        kernel_size = (5,9)
+        sigma = (0.1, 5.)
+        size = 100
+        batch_size = 4
+        num_workers = 2
+
+        cifar10 = TaskFactory.createTaskDataSet(task_name,root_dir,kernel_size,sigma,size,batch_size,num_workers)
+        assert isinstance(cifar10, CIFAR10Dataset)
