848f7b0359e3938c362b6c79b9349a1fa2fa4e47
stevendee37 <stevenwchang01@gmail.com>
2024-11-21 01:01:48 -0800

    Modified downsampling to use bicubic interpolation

--- test/test_data/test_factory.py
+++ test/test_data/test_factory.py
@@ -1,7 +1,9 @@
 import pytest
 import os
+
 from src.data.factory import TaskFactory
 from src.data.cifar10 import CIFAR10Dataset
+from src.data.imagenet import ImageNetDataset
 
 @pytest.mark.usefixtures("setUp")
 class TestTaskFactory():
@@ -12,9 +14,21 @@ class TestTaskFactory():
         root_dir = os.path.join(os.getcwd(), "datasets")
         blur_kernel_size = (5,9)
         sigma = (0.1, 5.)
-        size = 100
         batch_size = 32
         num_workers = 8
 
-        cifar10 = TaskFactory.createTaskDataSet(task_name,root_dir,blur_kernel_size,sigma,size,batch_size,num_workers)
+        cifar10 = TaskFactory.createTaskDataSet(task_name,root_dir,blur_kernel_size,sigma,batch_size=batch_size,num_workers=num_workers)
         assert isinstance(cifar10, CIFAR10Dataset)
+
+    def testImageNet(self, setUp):
+        """ Test that ImageNet object is instantiated correctly
+        """
+        task_name = "ImageNet"
+        root_dir = os.path.join(os.getcwd(), "datasets/imagenet")
+        blur_kernel_size = (5,9)
+        sigma = (0.1, 5.)
+        batch_size = 32
+        num_workers = 8
+
+        imagenet = TaskFactory.createTaskDataSet(task_name,root_dir,blur_kernel_size,sigma,batch_size=batch_size,num_workers=num_workers)
+        assert isinstance(imagenet, ImageNetDataset)
