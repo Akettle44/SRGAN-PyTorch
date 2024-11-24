1e76ef82505c9626300c4b578dce2427022b9951
akettle44 <andrewjkettle@gmail.com>
2024-11-23 19:06:45 -0800

    Adding ability to load and save model

---
+++ src/model/load_save.py
@@ -0,0 +1,63 @@
+import os
+import torch
+from .model import Generator, Discriminator
+from utils.utils import Utils
+
+def saveModelToDisk(generator, discriminator, root_dir, run_number):
+    """ Save the GAN model to disk
+
+    Args:
+        generator (pytorch nn.module): generator
+        discriminator (pytorch nn.module): discriminator
+        model_dir (str): Directory to save model to
+        run_number (int): Unique number to save the run files to
+    """
+
+    model_dir = os.path.join(os.path.join(root_dir, "models"), f"run{run_number}")
+    if( not os.path.exists(model_dir)):
+        os.mkdir(model_dir)
+
+    generator_dir = os.path.join(model_dir, "generator.pth")
+    discriminator_dir = os.path.join(model_dir, "discriminator.pth")
+
+    # Write the config, model, and tokenizer to disk
+    torch.save(generator.state_dict(), generator_dir)
+    torch.save(discriminator.state_dict(), discriminator_dir)
+
+    # Write model metadata to disk
+    # TODO: Make this relevant to GANs
+    #metadata = os.path.join(model_dir, "metadata.txt")
+    #with open(metadata, 'w') as f:
+        #f.write('Legend: num_classes, task_type \n')
+        #f.write(f"{model.num_classes}, {model.task_type}")
+
+def loadModelFromDisk(model_dir):
+    """ Load the model from disk locally
+
+    Args:
+        model_dir (str): path to model on disk
+    """
+
+    # Load Hyperparameters
+    task_name = model_dir.split('/')[-1].split('-')[0]
+    hyp_dir = os.path.dirname(os.path.dirname(model_dir)) # Insanely ratchet
+    hyps = Utils.loadHypsFromDisk(os.path.join(os.path.join(hyp_dir, 'hyps'), task_name + '.txt'))
+
+    #metadata = os.path.join(model_dir, "metadata.txt")
+    #with open(metadata, 'r') as f:
+    #    for idx, line in enumerate(f):
+    #        if idx == 1:
+    #            num_classes, task_type = line.split(',')
+    #            num_classes = int(num_classes)
+    #            task_type = task_type.strip()
+
+    # Load generator and discriminator
+    gen_state_dict = torch.load(os.path.join(model_dir, 'generator.pth'))
+    disc_state_dict = torch.load(os.path.join(model_dir, 'discriminator.pth'))
+
+    g = Generator()
+    g.load_state_dict(gen_state_dict)
+    d = Discriminator()
+    d.load_state_dict(disc_state_dict)
+
+    return g, d, hyps
